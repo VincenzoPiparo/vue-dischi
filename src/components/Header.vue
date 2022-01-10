4bae94450c5489ace38b4966bc57e0e269313be0
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-10 16:30:51 +0100

    END HEADER

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,10 +13,10 @@ export default {
 <style scoped lang="scss">
 @import '../style/variables';
 header{
-    background-color: $primary_color;
+   background-color: $primary_color;
         img{
-        width: 50px;
-        margin: 20px;
-    }
+            margin: 20px;
+            width: 50px;
+        }
 }
 </style>
