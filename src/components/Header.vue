1bc4a5785539bbcc9ade7d560168564ff2be10a0
VincenzoPiparo <v.pipar.97@gmai.com>
2022-01-10 16:25:52 +0100

    END HEADER

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+          <img src="../assets/img/spotify-logo.png" alt="Spotify Logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/variables';
+header{
+    background-color: $primary_color;
+        img{
+        width: 50px;
+        margin: 20px;
+    }
+}
+</style>
